e86c11c6361b3afe58aba74241cc5b59183844a4
AmilaNuwanW <amilawanasinghe400@gmail.com>
2024-05-08 01:54:09 +0530

    Changes Added

--- VehiclePortal.Web/Program.cs
+++ VehiclePortal.Web/Program.cs
@@ -26,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Vehicle}/{action=Details}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
